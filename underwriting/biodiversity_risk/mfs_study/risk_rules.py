a7a182282c06b05c9cbd6413d4cf9434a56a5f8e
Felipe Arruda Pontes <felipe.arrudapontes@maastrichtuniversity.nl>
2026-02-11 18:44:02 +0100

    new idea, only push higher the values for when PA is true

--- underwriting/biodiversity_risk/mfs_study/risk_rules.py
+++ underwriting/biodiversity_risk/mfs_study/risk_rules.py
@@ -14,8 +14,8 @@ new_rules.append(ctrl.Rule(
 rule_script = """
 new_rules = []
 """
-for ch_label in ['unknown', 'potential', 'likely']:
-    for pa_label in ['unprotected', 'protected']:
+for pa_label in ['unprotected', 'protected']:
+    for ch_label in ['unknown', 'potential', 'likely']:
         for si_label in reversed(['low', 'medium-low', 'medium', 'medium-high', 'high']):
             rule_script += rule_format.format(
                 ch_label=ch_label,
