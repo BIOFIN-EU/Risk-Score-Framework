07f548939450136f3586a607227263d26e866948
Felipe Arruda Pontes <felipe.arrudapontes@maastrichtuniversity.nl>
2026-03-27 19:14:29 +0100

    stoping in the middle of the run (need to do the mask crop now using polygon.

--- risk_framework/biodiversity_risk/model_wrapper.py
+++ risk_framework/biodiversity_risk/model_wrapper.py
@@ -1,5 +1,10 @@
 import numpy as np
 from risk_framework.web_api.utils import get_country_wkt, load_poligon_gdf
+import rasterio
+from rasterio.warp import reproject, Resampling, calculate_default_transform
+from rasterio.transform import from_origin
+from rasterio.mask import mask
+from shapely.geometry import mapping
 
 
 
@@ -65,10 +70,7 @@ class BiofinBiodiversityRiskModelWrapper(object):
         raster_array = np.array(reg_index_response.raster_data.raster, dtype=meta_dtype)
         return reg_index_response, raster_array, meta
 
-    def get_raster_and_meta_from_sri_response_object(self, climate_scenario, climate_model, period):
-        # (override_species_list, country_code, wkt_polygon, geo_id,
-        # climate_scenario, climate_model, period, logic_type, correction_method, db, future=False):
-        future = False # fix this
+    def get_raster_and_meta_from_sri_response_object(self, climate_scenario, climate_model, period, future):
         reg_index_response = self.sri_retrieval_method(
             self.sri_species_list, self.country_code, self.wkt_polygon, self.geo_id,
             climate_scenario, climate_model, period,
@@ -82,17 +84,76 @@ class BiofinBiodiversityRiskModelWrapper(object):
         raster_array = np.array(reg_index_response.raster_data.raster, dtype=meta_dtype)
         return reg_index_response, raster_array, meta
 
+    def align_rasters(self, rasters_list, metas_list):
+        """
+        Align all rasters to the reference raster shape and bounds
+        """
+        reference_meta = metas_list[0]
+        reference_raster = rasters_list[0]
+        h = reference_meta['height']
+        w = reference_meta['width']
+        aligned_rasters = []
+
+        # Create reference transform
+        ref_transform = from_origin(
+            reference_meta['transform'][2],
+            reference_meta['transform'][5],
+            reference_meta['transform'][0],
+            abs(reference_meta['transform'][4])
+        )
+        aligned_rasters.append(reference_raster)
+        for raster, meta in zip(rasters_list[1:], metas_list[1:]):
+            # Create destination array with reference shape
+            dest = np.full((h, w), -1, dtype=reference_raster.dtype)
+
+            # Create source transform
+            src_transform = from_origin(
+                meta['transform'][2],
+                meta['transform'][5],
+                meta['transform'][0],
+                abs(meta['transform'][4])
+            )
+
+            # Simple reprojection
+            reproject(
+                source=raster,
+                destination=dest,
+                src_transform=src_transform,
+                src_crs=meta['crs'],
+                src_nodata=-1,  # <-- ADD THIS
+                dst_transform=ref_transform,
+                dst_crs=reference_meta['crs'],
+                dst_nodata=-1,  # <-- ADD THIS
+                resampling=Resampling.bilinear
+            )
+            # Replace negative areas with reference raster values
+            dest[dest < 0] = reference_raster[dest < 0]
+            aligned_rasters.append(dest)
+
+        return aligned_rasters
+
     def run(self, climate_scenario, climate_model, period):
         print('Running RISK model.')
         # if current prediction
+        future = True
         if climate_scenario is None:
             climate_scenario = 'current'
             period = climate_scenario
+            future = False
 
-        species_sri_reg, species_hsi_raster, meta =
+        ch_reg, ch_raster, ch_meta = self.get_raster_and_meta_from_ch_response_object()
+        pa_reg, pa_raster, pa_meta = self.get_raster_and_meta_from_ch_response_object()
+        sri_reg, sri_raster, sri_meta = self.get_raster_and_meta_from_sri_response_object(
+            climate_model, climate_model, period, future)
 
-        default_meta = list_of_species_meta[0]
-        list_of_species_hsi_aligned = self.align_rasters(list_of_species_hsi, list_of_species_meta)
+        rasters_list = [sri_raster, ch_raster, pa_raster]
+        meta_list = [sri_meta, ch_meta, pa_meta]
+        default_meta = sri_meta
+        # not sure we need this... they should be all on the same projection...
+        # maybe just do a mask crop using the polygon
+        # create a validation_mask and pass it to the internal model as well.
+        # (only do operations when its valid vask for all component rasters)
+        list_of_species_hsi_aligned = self.align_rasters(rasters_list, meta_list)
 
         non_corrected_sri_raster = self.species_hsi_aggregation_method(list_of_species_hsi_aligned)
 
