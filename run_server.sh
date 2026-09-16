1c7223a92ae0e926cb734b57041d5a4175fe940e
Felipe Arruda Pontes <felipe.arrudapontes@maastrichtuniversity.nl>
2026-09-16 11:08:36 +0200

    log level properly set in runserver cmd

--- run_server.sh
+++ run_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo $WEB_PORT
-gunicorn -k uvicorn.workers.UvicornWorker risk_framework.web_api.core:app --bind 0.0.0.0:$WEB_PORT --workers 2 --timeout 0
+gunicorn -k uvicorn.workers.UvicornWorker risk_framework.web_api.core:app --bind 0.0.0.0:$WEB_PORT --workers 2 --timeout 0 --log-level $LOG_LEVEL
